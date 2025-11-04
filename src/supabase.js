b8e510eef4400c324810cdbde7fc831ea30d3af7
Giuseppe <cresctelli@gmail.com>
2025-11-04 09:08:45 +0100

    Fixed Supabase keys bug

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = import.meta.env.VITE_SUPABASE_URL;
-const KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const URL = "https://bmieklbextypaidmhqao.supabase.co";
+const KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtaWVrbGJleHR5cGFpZG1ocWFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3MTMzNjAsImV4cCI6MjA3NzI4OTM2MH0.4EuNnpIgbV79Lz4FwPXmDFz5Vj9DHhlBh1xIulesaVY";
 
 export const supabase = createClient(URL, KEY);
