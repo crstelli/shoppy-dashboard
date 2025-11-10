a7dc37ee02be91f63263c61380990f722be8de7c
Giuseppe <cresctelli@gmail.com>
2025-11-10 12:22:14 +0100

    UI/UX Improvement to last orders

--- src/features/dashboard/Order.jsx
+++ src/features/dashboard/Order.jsx
@@ -17,13 +17,13 @@ function Order({ order }) {
     tagColor = "red";
   }
   if (order.status === "received") {
-    bgColor = "bg-gray-100/75 dark:bg-gray-500/35";
+    bgColor = "bg-gray-100/75 dark:bg-gray-400/35";
     tagColor = "gray";
   }
 
   return (
     <div
-      className={`grid grid-cols-[1fr_3fr_3fr_3fr] items-center justify-items-center rounded-md px-4 py-2 text-sm dark:text-gray-900 ${bgColor}`}
+      className={`grid grid-cols-[1fr_3fr_3fr_3fr] items-center justify-items-center rounded-md px-4 py-2 text-sm dark:text-gray-300 ${bgColor}`}
     >
       <span className="justify-self-start">{order.id}</span>
       <span className="text-md text-gray-60 font-bold">${order.total}</span>
