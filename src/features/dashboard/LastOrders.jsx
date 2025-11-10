67d0a211997d6e0d3372f54d10e821357da97f57
Giuseppe <cresctelli@gmail.com>
2025-11-10 14:04:02 +0100

    Improved settings update

--- src/features/dashboard/LastOrders.jsx
+++ src/features/dashboard/LastOrders.jsx
@@ -1,6 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router";
+
 import { getOrders } from "../../services/apiOrders";
 import { Order } from "./Order";
+
+import { SquareArrowOutUpRight } from "lucide-react";
 
 function LastOrders() {
   const { data: orders } = useQuery({
@@ -12,7 +16,12 @@ function LastOrders() {
 
   return (
     <div className="flex h-full flex-col rounded-md bg-gray-50 p-4 dark:bg-gray-800">
-      <h3 className="text-xl font-semibold">Last orders</h3>
+      <div className="flex items-center justify-between">
+        <h3 className="text-xl font-semibold">Last orders</h3>
+        <Link to="/orders">
+          <SquareArrowOutUpRight />
+        </Link>
+      </div>
       <div className="mt-2 flex grow flex-col">
         <div className="grid grid-cols-[1fr_3fr_3fr_3fr] px-4 py-2 text-center font-bold">
           <span className="text-left">ID</span>
--- src/features/settings/Settings.jsx
+++ src/features/settings/Settings.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   getSettings,
   updateSettings as updateSettingsApi,
 } from "../../services/apiSettings";
 
 import { Spinner } from "../../shared/components/Spinner";
+import toast from "react-hot-toast";
 
 function Settings() {
-  const [deliveryPrice, setDeliveryPrice] = useState("");
+  const queryClient = useQueryClient();
 
   const { data: settings, isLoading } = useQuery({
     queryKey: ["settings"],
@@ -17,7 +18,14 @@ function Settings() {
 
   const { mutate: updateSettings } = useMutation({
     mutationFn: updateSettingsApi,
+
+    onSuccess: () => {
+      toast.success("Settings updated!");
+      queryClient.invalidateQueries("settings");
+    },
   });
+
+  const [deliveryPrice, setDeliveryPrice] = useState(settings.delivery_price);
 
   return isLoading ? (
     <Spinner />
@@ -27,16 +35,21 @@ function Settings() {
       <div className="col-span-2 mt-4 grid grid-cols-3 items-center gap-7 rounded-md border border-gray-300 bg-gray-50 p-6 dark:border-gray-700 dark:bg-gray-800">
         <label>Delivery Price</label>
         <input
-          className="rounded-md border border-gray-400 px-4 py-1 text-sm dark:border-gray-700"
+          className="rounded-md border border-gray-400 px-4 py-1 text-sm focus:border-emerald-500 focus:outline-none dark:border-gray-700"
           type="number"
-          value={deliveryPrice || settings?.delivery_price}
-          onChange={(e) => setDeliveryPrice(e.target.value)}
-          onBlur={() =>
-            updateSettings({
-              ...settings,
-              delivery_price: deliveryPrice,
-            })
-          }
+          value={deliveryPrice}
+          onChange={(e) => setDeliveryPrice(+e.target.value)}
+          onBlur={() => {
+            // if (deliveryPrice !== settings?.delivery_price)
+            //   console.log(Number(deliveryPrice), settings?.delivery_price);
+            // updateSettings({
+            //   ...settings,
+            //   delivery_price: deliveryPrice,
+            // });
+            if (deliveryPrice !== settings?.delivery_price) {
+              updateSettings({ ...settings, delivery_price: deliveryPrice });
+            }
+          }}
         />
         <div></div>
       </div>
